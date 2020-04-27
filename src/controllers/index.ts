1d7e0e65a12d7dc619c72a6be270d12014f8e9db
lleonesouza <lleonesouza@live.com>
2020-04-26 21:35:06 -0300

    first commit

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,39 @@
 import {} from "../use-cases/index";
+import {PrismaClient} from '@prisma/client'
 
-import makeHelloWorld from "./helloWorld/index";
+import patients from "./patients";
 
 const useCases = '<h1> hello world <h1/>'
+
+const api = new PrismaClient()
+
+
+const get = async () => {
+  const patient = await api.patient.findMany({first: 5})
+
+
+  const resource = JSON.parse(patient[0].resource)
+  console.log(resource.name)
+}
+
+
+get()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Inject Uses Cases Dependecies
 const getHelloWorld = makeHelloWorld({useCases});
